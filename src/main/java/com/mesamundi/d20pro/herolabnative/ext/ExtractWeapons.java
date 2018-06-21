95829647bac39492a9afb5068ae18403d22ba576
thraxxis <mat@mesamundi.com>
2018-06-21 10:18:20 -0600

    Handles multiple attacks (e.g. "Claw x2"); using simple keyword match for "bite", "tail" for 2 hand and "wing" for off-hand

--- src/main/java/com/mesamundi/d20pro/herolabnative/ext/ExtractWeapons.java
+++ src/main/java/com/mesamundi/d20pro/herolabnative/ext/ExtractWeapons.java
@@ -3,7 +3,6 @@ package com.mesamundi.d20pro.herolabnative.ext;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.regex.Pattern;
 
@@ -18,12 +17,10 @@ import com.mindgene.d20.common.creature.CreatureTemplate;
 import com.mindgene.d20.common.creature.attack.*;
 import com.mindgene.d20.common.dice.Dice;
 import com.mindgene.d20.common.dice.DiceFormatException;
-import com.mindgene.d20.common.item.ItemTemplate;
 import com.mindgene.d20.dm.game.CreatureInPlay;
 
 import static com.d20pro.plugin.api.XMLToDocumentHelper.xpath;
 import static com.mesamundi.d20pro.herolabnative.HeroLabNativeImportLogic.characterPath;
-import static com.mesamundi.d20pro.herolabnative.HeroLabNativeImportLogic.resolveBAB;
 import static com.mesamundi.d20pro.herolabnative.arc.NamedItemArc.arc;
 import static com.mesamundi.d20pro.herolabnative.arc.NamedItemArc.trav;
 
@@ -50,9 +47,54 @@ public class ExtractWeapons implements DependentExtractor {
     for (int i = 0, len = nodes.getLength(); i < len; i++) {
       CreatureAttack weapon = extractWeapon(nodes.item(i), nug);
       attacks.add(weapon);
-    }
 
-    // TODO: 7/18/2017 handle rules specific attacks like combat maneuver for pathfinder
+      // TODO: 7/18/2017 handle rules specific attacks like combat maneuver for pathfinder
+
+      // consider multiple like "Wing x2 (Great Wyrm Blue Dragon)"
+      String name = weapon.getName();
+      int num = resolveNumSiblings(name);
+      if(num > 1)
+      {
+        String suffix = " x" + num;
+        name = name.substring(0, name.length() - suffix.length());
+        weapon.setName(name);
+        for(int j = 1; j < num; j++)
+        {
+          weapon = extractWeapon(nodes.item(i), nug);
+          weapon.setName(name);
+          attacks.add(weapon);
+        }
+      }
+    }
+  }
+
+  private static String removeParens(String name)
+  {
+    if(name.endsWith(")"))
+    {
+      int at = name.lastIndexOf(" (");
+      if(at > -1)
+        return name.substring(0, at);
+    }
+    return name;
+  }
+
+  private int resolveNumSiblings(String name)
+  {
+    String[] split = name.split(" ");
+    String end = split[split.length - 1];
+    if(end.startsWith("x"))
+    {
+      end = end.substring(1);
+      try {
+        return Integer.parseInt(end);
+      }
+      catch (NumberFormatException nfe)
+      {
+        lg.error("Failed to parse int from: " + end);
+      }
+    }
+    return 1;
   }
 
   @Override
@@ -65,7 +107,7 @@ public class ExtractWeapons implements DependentExtractor {
     atk.assumeType(type);
 
     trav(node,
-            arc("name", atk::setName),
+            arc("name", name(atk)),
             arc("typetext", typetext(nug.ctr, atk)),
             arc("attack", attack(nug, atk)),
             arc("crit", crit(atk)),
@@ -74,6 +116,11 @@ public class ExtractWeapons implements DependentExtractor {
     );
 
     return atk;
+  }
+
+  private static Consumer<String> name(CreatureAttack atk)
+  {
+    return v -> atk.setName(removeParens(v));
   }
 
   private static Consumer<String> typetext(CreatureTemplate ctr, CreatureAttack atk) {
@@ -86,7 +133,19 @@ public class ExtractWeapons implements DependentExtractor {
   private static Consumer<String> attack(Nug nug, CreatureAttack atk) {
     return v -> {
       String[] raw = v.split(Pattern.quote("/"));
-      atk.setAttackCascading(raw.length > 1);
+      if(raw.length > 1) {
+        // there are more than one attack, check if they are equal
+        int val = Integer.parseInt(raw[0]);
+        boolean same = true;
+        for(int i = 1; i < raw.length; i++)
+          if(val != Integer.parseInt(raw[i]))
+          {
+            same = false;
+            break;
+          }
+        if(!same)
+          atk.setAttackCascading(true);
+      }
 
       int resolvedToHit = Integer.parseInt(raw[0]);
 
@@ -135,16 +194,20 @@ public class ExtractWeapons implements DependentExtractor {
         throw new RuntimeException("Failed to parse primary damage: " + raw[0]);
       }
 
-      // look for the corresponding item
+      atk.setStyle(ascertainStyle(atk.getName()));
+
+//      // look for the corresponding item
       ExtractGear.findItem(atk.getName(), nug).ifPresent(item -> {
-        float weight = item.accessWeight();
-        if(weight > 8)
-          atk.setStyle(new CreatureAttackStyle_2Hand());
+        // if no item,
+//        float weight = item.accessWeight();
+//        if(weight > 8)
+//          atk.setStyle(new CreatureAttackStyle_2Hand());
       });
 
-      // TODO account for 2 hand or off hand
-      byte mod = nug.ctr.getAbilityScoreMods(atk.getAbilityToDamage());
-      dice.setMod(dice.getMod() - mod);
+      // store the mod for later
+      int originalMod = dice.getMod();
+      // zero it out for now
+      dice.setMod(0);
 
       CreatureAttackDamage dmg = new CreatureAttackDamage();
       dmg.setDice(dice);
@@ -164,7 +227,33 @@ public class ExtractWeapons implements DependentExtractor {
       }
       dmg.setAttackQualities(qualities);
       damages.add(dmg);
+
+      DeclaredCreatureAttack attack = new DeclaredCreatureAttack(atk, 0, new CreatureInPlay(nug.ctr));
+      ArrayList<CreatureAttackDamage> resolvedDamages = attack.resolveDamages(nug.dm());
+      Dice resolvedDice = resolvedDamages.get(damages.size() - 1).getDice();
+      dice.setMod(originalMod - resolvedDice.getMod());
     };
+  }
+
+  private static CreatureAttackStyle ascertainStyle(String name)
+  {
+    String[] twos = { "bite", "tail"};
+    String[] offs = { "wing" };
+
+    String lowerName = name.toLowerCase();
+    for(String two : twos)
+      if(lowerName.contains(two)) {
+        lg.debug("Found 2Hand keyword: " + two + " in: " + name);
+        return new CreatureAttackStyle_2Hand();
+      }
+
+    for(String off : offs)
+      if(lowerName.contains(off)) {
+        lg.debug("Found OffHand keyword: " + off + " in: " + name);
+        return new CreatureAttackStyle_OffHand();
+      }
+
+    return new CreatureAttackStyle_1Hand();
   }
 
   private static Consumer<String> equipped(CreatureTemplate ctr, CreatureAttack atk) {
